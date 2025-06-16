a83faafcb1b003342f9c602e899730079387781f
Abhishek Anand <bgsabhishekanand02@gmail.com>
2025-06-16 08:01:24 +0530

    Majority element -Tc = O(nlogn)*n

--- Array/Medium/Ques3.cpp
+++ Array/Medium/Ques3.cpp
@@ -69,3 +69,32 @@ int majorityElement(vector<int>& arr) {
         }
         return -1;
     }
+
+//Solution - Tc = O(nlogn)*n , sc = o(1)
+int majorityElement(vector<int>& arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        // freq count 
+        int freq= 1; 
+        int ans  = arr[0];
+        
+        if(n == 1){
+            return ans;
+        }
+        
+        
+        for(int i=1;i<arr.size();i++){
+            if(arr[i] == arr[i-1]){
+                freq++;
+            }
+            else{
+                freq=1;
+                ans = arr[i];
+            }
+            if(freq >n/2 ){
+                return ans;
+            }
+        }
+        return -1;
+    }
